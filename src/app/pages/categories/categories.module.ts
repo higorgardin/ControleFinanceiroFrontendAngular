6729db1c42f6f2a9eb8b9adc4a572a3b9d19337f
Higor Gardin <higorgardin00@gmail.com>
2022-06-02 22:17:25 -0300

    Created categories component

---
+++ src/app/pages/categories/categories.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { CategoriesRoutingModule } from './categories-routing.module';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryService } from './shared/category.service';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  declarations: [CategoryListComponent, CategoryFormComponent],
+  imports: [SharedModule, CategoriesRoutingModule],
+  providers: [CategoryService],
+})
+export class CategoriesModule {}
